bf06818319e335228d7f298354772b25d06f50f9
StevenS125 <sts12589@gmail.com>
2018-03-16 17:34:05 -0400

    lifecycles logged

--- src/App.js
+++ src/App.js
@@ -1,21 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logify from './logify';
 
 class App extends Component {
+
+static displayName = "App"
+
+state = {
+  data: "no data yet"
+}
+
+  fetchData = () => {
+    console.log("going to fetch data")
+    setTimeout(
+      () => {
+        console.log("data retrieved")
+        this.setState({
+          data: Math.random()
+        })
+      },
+      1500
+    )
+  }
+
+  componentDidMount() {
+    this.fetchData()
+    const canvasCtx = this.refs.appCanvas.getContext('2d')
+    canvasCtx.fillStyle="blue"
+    canvasCtx.arc(75,75,50,0,2 * Math.PI)
+    canvasCtx.fill()
+  }
+
+
   render() {
+    let {showPollChild} = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+<div> Hello World!
+
+  <h4>{this.state.data}</h4>
+
+<canvas 
+ref={"appCanvas"}
+height={200}
+width={200}
+/>
+
+<button onClick={()=> {
+  this.setState((prevState) => {
+    return {
+      showPollChild: !prevState.showPollChild
+       }
+     })
+  }}
+>
+{(showPollChild) ? "Hide" : "Show"} 
+</button>
+{(showPollChild) ? <PollChild/> : null} 
+</div>
+
     );
   }
 }
+
+class PollChild extends Component {
+
+static displayName = "PollChild"
+
+state = {
+  poll: Math.random()
+}
+
+componentDidMount() {
+  this.polldData()
+}
+
+componentWillUnmount() {
+  clearInterval(this.pollInterval)
+}
+
+  polldData = () => {
+    this.pollInterval = setInterval(() => {
+      console.log("Poll!")
+      this.setState({ 
+        poll: Math.random()
+      })
+    }, 1000
+  )
+  }
+
+   render() {
+     return (
+       <h4> poll: {this.state.poll}</h4>
+     )
+   }
+}
+
+App = logify(App)
+PollChild = logify(PollChild)
 
 export default App;
